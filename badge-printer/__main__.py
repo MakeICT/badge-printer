31d8f6cca56e4e8d58b6296cedd2c0ecf4262737
Dominic Canare <dom@makeict.org>
2017-08-04 18:06:15 -0500

    camera works

--- badge-printer/__main__.py
+++ badge-printer/__main__.py
@@ -5,11 +5,13 @@ import os, sys, traceback, io
 import base64
 from functools import partial
 
-from PyQt5 import QtCore, QtGui, QtWidgets, QtPrintSupport, QtSvg, uic, QtWebEngineWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5 import QtWebEngineWidgets, QtPrintSupport, QtMultimediaWidgets
+from PyQt5 import QtMultimedia
 
 import pyqrcode
 
-#from V4L2
+#d V4L2
 
 CHOOSE_CUSTOM = object()
 RELOAD = object()
@@ -19,14 +21,24 @@ class BadgePrinterApp(QtWidgets.QApplication):
 		super().__init__(args)
 		self.basePath = os.path.dirname(os.path.realpath(__file__))
 		self.templateFilename = None
+		self.camera = None
 
 		self.mainWindow = uic.loadUi(self._path('MainWindow.ui'))
+		self.mainWindow.previewTabs.tabBar().hide()
+		
+		self.cameraViewFinder = QtMultimediaWidgets.QCameraViewfinder(self.mainWindow.cameraTab)
+		self.mainWindow.cameraTab.layout().addWidget(self.cameraViewFinder)
+
+		preview = self.mainWindow.preview
+		preview.page().setBackgroundColor(
+			preview.palette().color(preview.backgroundRole())
+		)
 
 		self.mainWindow.actionImport.triggered.connect(self.attemptImport)
 		self.mainWindow.actionLoadTemplate.triggered.connect(self.browseForTemplate)
 		self.mainWindow.actionSaveACopy.triggered.connect(self.saveACopy)
 
-		self.mainWindow.actionCapture.triggered.connect(self.capture)
+		self.mainWindow.actionCapture.triggered.connect(self.captureToggle)
 		self.mainWindow.actionPrint.triggered.connect(self.attemptPrint)
 
 		self.mainWindow.actionExit.triggered.connect(self.exit)
@@ -83,28 +95,59 @@ class BadgePrinterApp(QtWidgets.QApplication):
 		except Exception as exc:
 			unhandledError(exc, self.mainWindow)
 
-	def capture(self):
+	def captureToggle(self):
 		try:
-			raise NotImplementedError()
+			tabs = self.mainWindow.previewTabs
+			if tabs.currentWidget() == self.mainWindow.badgePreviewTab:
+				tabs.setCurrentWidget(self.mainWindow.waitTab)
+
+				if self.camera is None:
+					cameraInfo = QtMultimedia.QCameraInfo.availableCameras()[0]
+					self.camera = QtMultimedia.QCamera(cameraInfo)
+					self.camera.viewfinderSettings().setResolution(640,480)
+					self.camera.setViewfinder(self.cameraViewFinder)
+					self.cameraViewFinder.setAspectRatioMode(QtCore.Qt.KeepAspectRatio)
+
+					def statusChanged(status):
+						if status == QtMultimedia.QCamera.ActiveStatus:
+							tabs.setCurrentWidget(self.mainWindow.cameraTab)
+
+					self.camera.statusChanged.connect(statusChanged)
+				self.camera.start()
+
+			elif tabs.currentWidget() == self.mainWindow.cameraTab:
+				def imageSaved(id, filename):
+					self.useImage(filename)
+
+				self.imageCapture = QtMultimedia.QCameraImageCapture(self.camera)
+				self.imageCapture.imageSaved.connect(imageSaved)
+				tmpFile = os.path.abspath(os.path.join('archive', '_capture.jpg'))
+				self.imageCapture.capture(tmpFile)
+				self.camera.stop()
+
+		except Exception as exc:
+			unhandledError(exc, self.mainWindow)
+
+	def useImage(self, filename):
+		try:
+			with open(filename, 'rb') as captureFile:
+				self.updateImage('photo', captureFile.read(), 'jpeg')
 		except Exception as exc:
 			unhandledError(exc, self.mainWindow)
 
 	def attemptPrint(self):
 		try:
+			raise NotImplementedError()
+
 			printer = QtPrintSupport.QPrinter()
 			dialog = QtPrintSupport.QPrintDialog(printer, self.mainWindow)
 			if dialog.exec_() != QtWidgets.QDialog.Accepted:
 				return
 			
 			painter = QtGui.QPainter(printer)
-			#painter.begin(printer)
-			with open(self.templateFilename) as svgFile:
-				svgRenderer = QtSvg.QSvgRenderer(svgFile.read())
-			
-			svgRenderer.render(painter)
+			painter.begin(printer)
 			painter.end()
 
-			self.mainWindow.setStyleSheet(styleSheet)
 		except Exception as exc:
 			unhandledError(exc, self.mainWindow)
 
@@ -146,12 +189,10 @@ class BadgePrinterApp(QtWidgets.QApplication):
 			unhandledError(exc, self.mainWindow)
 
 	def _contentLoaded(self):
-		self.mainWindow.preview.setZoomFactor(1)
 		self._updatePreview()
 		self.autoScale()
 
 	def autoScale(self):
-
 		preview = self.mainWindow.preview
 		def haveDocumentSize(size):
 			if 0 in size:
@@ -162,6 +203,14 @@ class BadgePrinterApp(QtWidgets.QApplication):
 			heightRatio = preview.height() / contentSize.height()
 			scale = min(widthRatio, heightRatio)
 			preview.setZoomFactor(scale)
+			# give a little padding
+			preview.setZoomFactor(.9*scale)
+
+			# try to vertically center...
+			preview.page().runJavaScript('''
+				document.documentElement.style.margin = "auto";
+				document.documentElement.style.marginTop = "%dpx";
+			''' % int((preview.height() - scale*contentSize.height())))
 
 		js = '[document.documentElement.scrollWidth, document.documentElement.scrollHeight]'
 		preview.page().runJavaScript(js, haveDocumentSize)
@@ -217,31 +266,42 @@ class BadgePrinterApp(QtWidgets.QApplication):
 		buffer = io.BytesIO()
 		qr = pyqrcode.create(self.mainWindow.qrInput.text())
 		qr.png(buffer, scale=10, quiet_zone=0)
-		data = 'data:image/png;base64,%s' % base64.b64encode(buffer.getvalue()).decode('ascii')
+		self.updateImage('qr', buffer.getvalue(), 'png')
+	
+	#	type should be "png" or "jpeg"
+	def updateImage(self, id, rawData, imageType):
+		def goToPreview(dummy=None):
+			self.mainWindow.previewTabs.setCurrentWidget(self.mainWindow.badgePreviewTab)
+
+		data = 'data:image/%s;base64,%s' % (imageType, base64.b64encode(rawData).decode('ascii'))
 		js = '''
-			var el = document.getElementById("qr");
+			var el = document.getElementById("%s");
 			if(el) el.setAttribute("xlink:href", "%s");
 		'''
-		self.mainWindow.preview.page().runJavaScript(js % data)
+		self.mainWindow.preview.page().runJavaScript(js % (id, data), goToPreview)
 
 def unhandledError(exc, parent=None):
-	stack = traceback.format_exc()
-	print(stack)
-
 	dialog = QtWidgets.QMessageBox(parent)
 	dialog.setWindowTitle('Error :(')
-	dialog.setText('Well this is embarrasing... Something went wrong and I didn\'t know how to handle it.\n\nPlease help us fix this bug!')
-	dialog.setDetailedText(
-		'The details below can help troubleshoot the issue. Please copy-and-paste this in any report.\n\n' +
-		'Developer info at http://github.com/makeict/badge-printer\n\n' + 
-		'%s' % stack
-	)
+	if isinstance(exc, NotImplementedError):
+		dialog.setText('Sorry! This feature isn\'t implemented yet :(\n\nWanna help? Visit http://github.com/makeict/badge-printer')
+	else:
+		stack = traceback.format_exc()
+		print(stack)
+		dialog.setText('Well this is embarrasing... Something went wrong and I didn\'t know how to handle it.\n\nPlease help us fix this bug!')
+		dialog.setDetailedText(
+			'The details below can help troubleshoot the issue. Please copy-and-paste this in any report.\n\n' +
+			'Developer info at http://github.com/makeict/badge-printer\n\n' + 
+			'%s' % stack
+		)
+
 	dialog.setModal(True)
 	dialog.exec_()
 
-
 if __name__ == '__main__':
 	try:
+		os.makedirs('archive', exist_ok=True)
+
 		app = BadgePrinterApp(sys.argv)
 		app.doItNowDoItGood()
 	except Exception as exc:

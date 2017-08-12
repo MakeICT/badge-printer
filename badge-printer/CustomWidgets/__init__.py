a3653dc43a028274a3e4444e1815f8dca4fdbc73
Dominic Canare <dom@makeict.org>
2017-08-12 17:28:01 -0500

    resize templates

--- badge-printer/CustomWidgets/__init__.py
+++ badge-printer/CustomWidgets/__init__.py
@@ -11,12 +11,10 @@ class WebViewer(QtWebEngineWidgets.QWebEngineView):
 		self.loadFinished.connect(self._contentLoaded)
 		self._css = '''
 			<style>
-				svg {
-					margin: auto;
-				}
 				@media screen {
 					svg {
 						background: ''' + self.palette().color(self.backgroundRole()).name() + ''';
+						margin: auto;
 						margin-top: 2%;
 						height: 96%;
 						width: auto;
